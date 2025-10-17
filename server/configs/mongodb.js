0eb1671b971ff7faaf5250ffa95ce2ae455aa5e8
Kamal1701 <kk17kannan@yahoo.com>
2025-10-17 12:32:08 +0530

    added user model

---
+++ server/configs/mongodb.js
@@ -0,0 +1,24 @@
+// import mongoose from "mongoose";
+const mongoose = require("mongoose");
+
+// Connect to the MongoDB database
+
+const connectDB = async () => {
+  mongoose.connection.on("connected", () => console.log("Database Connected"));
+
+  await mongoose.connect(`${process.env.MONGODB_URI}/lms`);
+};
+
+// export default connectDB;
+
+// const mongoose = require("mongoose");
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGODB_URI);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+module.exports = connectDB;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./configs/mongodb");
+// const mongoose = require("mongoose");
 
 // Initialize express
 const app = express();
+
+// connect to database
+connectDB();
 
 // Middleware
 app.use(cors());
@@ -18,3 +23,10 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+// mongoose.connection.once("open", () => {
+//   console.log("Connected to MongoDB");
+//   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// });
+
+// mongoose.connection.on("error", (err) => console.log(err));
